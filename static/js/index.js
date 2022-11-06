fc181549560d12a52ecdea7bbcdf0dd7f166118c
Dridi Yassin <73611344+yaslack@users.noreply.github.com>
2022-11-06 18:28:03 +0100

    update

--- static/js/index.js
+++ static/js/index.js
@@ -4,4 +4,19 @@ function changeColor(){
 input=document.querySelector('[name="url"]')
 input.style.width = input.value.length = input.getAttribute('placeholder').length-12+"ch";
 
+var x = document.getElementById("loadingSubmit");
+x.style.display = "none";
 
+function validate() {
+
+    var y = document.getElementById("buttonSubmit");
+    y.style.display = "none";
+    var x = document.getElementById("loadingSubmit");
+    x.style.display = "block";
+      
+}
+
+function submitForm(){
+    validate()
+    document.getElementById("theForm").submit();
+}
