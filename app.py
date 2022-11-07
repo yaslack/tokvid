8ca1b528fe1dc4e217b1407e517220f5efba6ad1
Dridi Yassin <73611344+yaslack@users.noreply.github.com>
2022-11-07 04:35:25 +0100

    u

--- app.py
+++ app.py
@@ -11,23 +11,15 @@ path="static/url/"
 
 async def dynamic_page(url):
     try:
-        print("la")
-    
         api =  TikTokApi()
-        print("la")
-
-        print(url)
         data =  api.video(url=url).info()
-        print("la")
-        # Bytes of the TikTok video
         data=str(data)
-        print("la")
         element = data.split("'UrlList': ['",1)[1]
         video_url = element.split("'",1)[0]
         element = data.split("'createTime':",1)[1]
         desc = element.split(", 'desc': ",1)[1].split(',',1)[0]
         cover = data.split("'cover': '",1)[1].split("'",1)[0]
-        print("la")
+        
         return (video_url,desc,cover)
     except BaseException as err:
         print(err)
