711d6ec8533b9391c9624da499c9af05fc5ca948
Dridi Yassin <73611344+yaslack@users.noreply.github.com>
2022-11-11 15:28:09 +0100

    y

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ path="static/url/"
 
 @app.route('/robots.txt')
 def noindex():
-    r = Response(response="User-Agent: *\nDisallow: /\n", status=200, mimetype="text/plain")
+    r = Response(response="User-Agent: *\Allow: /\n", status=200, mimetype="text/plain")
     r.headers["Content-Type"] = "text/plain; charset=utf-8"
     return r
 
